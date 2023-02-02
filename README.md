b9894129f59df1d5ffb4e1771ecc1d307a83eeff
Wael Ramadan <thebigguyis@gmail.com>
2023-02-02 23:22:32 +0200

    add env

--- README.md
+++ README.md
@@ -16,8 +16,18 @@ source venv/bin/activate
 pip3 install -r requirements.txt
 ```
 
-2. Run django server:
+2. Copy `.env-example` to `.env`:
+```bash
+cp .env-example .env
+```
+
+3. Run django server:
 ```bash
 cd lxdash/
 python3 manage.py runserver
+```
+
+## Generate a New Django Secret Key with the following:
+```bash
+python3 -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())'
 ```
--- lxdash/lxdash/settings.py
+++ lxdash/lxdash/settings.py
@@ -10,17 +10,25 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+import dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PROJECT_DIR = Path(__file__).resolve().parent.parent.parent
+
+# Load .env file
+dotenv_file = os.path.join(PROJECT_DIR, ".env")
+print("BASE_DIR: ", dotenv_file)
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r1gdk90n-+0kgy%s_bzo=4=@k67rhbp2qn22@6f&$hr*0-*)q9'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
